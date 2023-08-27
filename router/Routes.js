c6f587c8d9a0243187bda6c5e8b809883b242cd7
Rahul <caulirahul6@gmail.com>
2023-08-27 23:43:20 +0530

    Controller setup

--- router/Routes.js
+++ router/Routes.js
@@ -1,85 +1,14 @@
 const express = require("express");
+const {
+  postProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+} = require("../controller/CRUDcontroller");
 const router = express.Router();
-const Product = require('../db/Schema')
 
-router.post("/v1/product/new", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-
-    res.status(201).json({
-      success: true,
-      product,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-router.get("/v1/products", async (req, res) => {
-  try {
-    const products = await Product.find();
-
-    res.status(200).json({
-      success: true,
-      products,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-router.put("/v1/product/:id", async (req, res) => {
-  try {
-    let product = await Product.findById(req.params.id);
-
-    if (!product) {
-      return res.status(404).json({
-        success: false,
-        message: "Product not found!",
-      });
-    }
-
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      useFindAndModify: false,
-      runValidators: true,
-    });
-
-    res.status(200).json({
-      success: true,
-      product,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-router.delete("/v1/product/:id", async (req, res) => {
-  try {
-    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
-
-    if (!deletedProduct) {
-      return res.status(404).json({
-        success: false,
-        message: "Product not found!",
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      message: "Product is deleted successfully",
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: error.message,
-    });
-  }
-});
+router.post("/v1/product/new", postProduct);
+router.get("/v1/products", getProducts);
+router.put("/v1/product/:id", updateProduct);
+router.delete("/v1/product/:id", deleteProduct);
 module.exports = router;
