24bff9dc4f1f8a1debc316c54636eb99455d40d4
Harsh Vardhan Pandey <hp424805@gmail.com>
2023-09-26 02:14:18 +0530

    Revert ":sparkles:refactor CRUDcontroller and added middleware"

--- db/Schema.js
+++ db/Schema.js
@@ -4,9 +4,7 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String,
   price: { type: Number, required: true },
-},
-  { timestamps: true },
-);
+});
 
 const Product = new mongoose.model("Product", productSchema);
 module.exports = Product;
